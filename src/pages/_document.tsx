e7f26a065dd87fe46474cfd953b048820c75620b
ishan-singh-3005 <ishan423@gmail.com>
2024-10-07 22:26:58 -0400

    added live chat

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-
 import { AppConfig } from '../utils/AppConfig';
 
 // Need to create a custom _document because i18n support is not compatible with `next export`.
@@ -7,7 +6,24 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang={AppConfig.locale}>
-        <Head />
+        <Head>
+          {/* Tawk.to Live Chat Script */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+                (function(){
+                  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                  s1.async=true;
+                  s1.src='https://embed.tawk.to/6704384ecec6d0125df2e02a/1i9k7nsfu';
+                  s1.charset='UTF-8';
+                  s1.setAttribute('crossorigin','*');
+                  s0.parentNode.insertBefore(s1,s0);
+                })();
+              `,
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
